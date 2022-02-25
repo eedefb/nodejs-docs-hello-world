37c25ebbd29047f3af68dbd44c3d0e52f9438ab0
eedefb <61330637+eedefb@users.noreply.github.com>
2022-02-24 21:38:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hola Mundo usando docker");
+    response.end("Hola Mundo usando docker con otro ajuste");
 });
 
 const port = process.env.PORT || 1337;
